52f864ae69cc5472b13732ba1609dfb5d851cb75
navilap <navilap@unal.edu.co>
2021-12-06 12:00:52 -0500

    Se agregan Jets R10

--- 3.Jets/Jets_macro.C
+++ 3.Jets/Jets_macro.C
@@ -25,10 +25,15 @@ void Jets_macro()
   TCanvas *canvas = new TCanvas("Canvas", "", 800, 600);
 
   // Define histograms
-  TH1F *hist_leadreco_pt_evtw = new TH1F("Lead Reco-jet", "Leading jet pT; pT(GeV);Events", 50, 10, 200);
-  TH1F *hist_leadtruth_pt_evtw = new TH1F("Lead Truth-jet", "Leading jet pT; pT(GeV);Events", 50, 10, 200);
-  TH1F *hist_leadreco_pt = new TH1F("Lead Reco-jet", "Leading jet pT; pT(GeV);Events", 50, 10, 300);
-  TH1F *hist_leadtruth_pt = new TH1F("Lead Truth-jet", "Leading jet pT; pT(GeV);Events", 50, 10, 300);
+  TH1F *hist_leadreco_R4_pt_evtw = new TH1F("Lead Reco-jet", "Leading jet pT; pT(GeV);Events", 50, 10, 200);
+  TH1F *hist_leadtruth_R4_pt_evtw = new TH1F("Lead Truth-jet", "Leading jet pT; pT(GeV);Events", 50, 10, 200);
+  TH1F *hist_leadreco_R4_pt = new TH1F("Lead Reco-jet", "Leading jet pT; pT(GeV);Events", 50, 10, 300);
+  TH1F *hist_leadtruth_R4_pt = new TH1F("Lead Truth-jet", "Leading jet pT; pT(GeV);Events", 50, 10, 300);
+
+  TH1F *hist_leadreco_R10_pt_evtw = new TH1F("Lead Reco-jet", "Leading jet pT; pT(GeV);Events", 50, 80, 500);
+  TH1F *hist_leadtruth_R10_pt_evtw = new TH1F("Lead Truth-jet", "Leading jet pT; pT(GeV);Events", 50, 80, 500);
+  TH1F *hist_leadreco_R10_pt = new TH1F("Lead Reco-jet", "Leading jet pT; pT(GeV);Events", 50, 80, 500);
+  TH1F *hist_leadtruth_R10_pt = new TH1F("Lead Truth-jet", "Leading jet pT; pT(GeV);Events", 50, 80, 500);
 
   // Fill histograms
   int nentries, nbytes, i;
@@ -41,35 +46,61 @@ void Jets_macro()
 
     if (reco_R4_pt->size() > 0)
     {
-      hist_leadreco_pt_evtw->Fill(reco_R4_pt->at(0) / 1000., evtw);
-      hist_leadreco_pt->Fill(reco_R4_pt->at(0) / 1000.);
+      hist_leadreco_R4_pt_evtw->Fill(reco_R4_pt->at(0) / 1000., evtw);
+      hist_leadreco_R4_pt->Fill(reco_R4_pt->at(0) / 1000.);
     }
 
     if (truth_R4_pt->size() > 0)
     {
-      hist_leadtruth_pt_evtw->Fill(truth_R4_pt->at(0) / 1000., evtw);
-      hist_leadtruth_pt->Fill(truth_R4_pt->at(0) / 1000.);
+      hist_leadtruth_R4_pt_evtw->Fill(truth_R4_pt->at(0) / 1000., evtw);
+      hist_leadtruth_R4_pt->Fill(truth_R4_pt->at(0) / 1000.);
+    }
+
+    if (reco_R10_pt->size() > 0)
+    {
+      hist_leadreco_R10_pt_evtw->Fill(reco_R10_pt->at(0) / 1000., evtw);
+      hist_leadreco_R10_pt->Fill(reco_R10_pt->at(0) / 1000.);
+    }
+
+    if (truth_R10_pt->size() > 0)
+    {
+      hist_leadtruth_R10_pt_evtw->Fill(truth_R10_pt->at(0) / 1000., evtw);
+      hist_leadtruth_R10_pt->Fill(truth_R10_pt->at(0) / 1000.);
     }
   }
 
   std::cout << "Done!" << std::endl;
 
   // Print histograms
-  hist_leadtruth_pt_evtw->SetMarkerStyle(20);
-  hist_leadtruth_pt_evtw->SetMarkerColor(1);
-  hist_leadtruth_pt_evtw->Draw("");
-  hist_leadreco_pt_evtw->SetMarkerStyle(20);
-  hist_leadreco_pt_evtw->SetMarkerColor(2);
-  hist_leadreco_pt_evtw->Draw("same");
+  hist_leadtruth_R4_pt_evtw->SetMarkerStyle(20);
+  hist_leadtruth_R4_pt_evtw->SetMarkerColor(1);
+  hist_leadtruth_R4_pt_evtw->Draw("");
+  hist_leadreco_R4_pt_evtw->SetMarkerStyle(20);
+  hist_leadreco_R4_pt_evtw->SetMarkerColor(2);
+  hist_leadreco_R4_pt_evtw->Draw("same");
   canvas->SetLogy();
   canvas->Print("Jets_R4_evtw.pdf");
-  canvas->Clear();
 
-  hist_leadtruth_pt->SetLineColor(1);
-  hist_leadtruth_pt->Draw("");
-  hist_leadtruth_pt->SetLineColor(2);
-  hist_leadreco_pt->Draw("same");
+  hist_leadtruth_R4_pt->SetLineColor(1);
+  hist_leadtruth_R4_pt->Draw("");
+  hist_leadreco_R4_pt->SetLineColor(2);
+  hist_leadreco_R4_pt->Draw("same");
   canvas->SetLogy();
   canvas->Print("Jets_R4.pdf");
-  canvas->Clear();
+
+  hist_leadtruth_R10_pt_evtw->SetMarkerStyle(20);
+  hist_leadtruth_R10_pt_evtw->SetMarkerColor(1);
+  hist_leadtruth_R10_pt_evtw->Draw("");
+  hist_leadreco_R10_pt_evtw->SetMarkerStyle(20);
+  hist_leadreco_R10_pt_evtw->SetMarkerColor(2);
+  hist_leadreco_R10_pt_evtw->Draw("same");
+  canvas->SetLogy();
+  canvas->Print("Jets_R10_evtw.pdf");
+
+  hist_leadtruth_R10_pt->SetLineColor(1);
+  hist_leadtruth_R10_pt->Draw("");
+  hist_leadreco_R10_pt->SetLineColor(2);
+  hist_leadreco_R10_pt->Draw("same");
+  canvas->SetLogy();
+  canvas->Print("Jets_R10.pdf");
 }
