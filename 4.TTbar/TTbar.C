3a8c8c686289885732c11ce64243df5ba9001d79
navilap <navilap@unal.edu.co>
2022-01-24 00:36:07 -0500

    Tarea TTbar

---
+++ 4.TTbar/TTbar.C
@@ -0,0 +1,353 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+void TTbar()
+{
+    //ROOt File
+    TFile *file = TFile::Open("Data_8TeV.root");
+
+    // Tree and print
+    TTree *tree = (TTree *)file->Get("mini");
+    tree->Print();
+
+    //Variables
+    Bool_t e_trig;
+    Bool_t mu_trig;
+    Bool_t good_vtx;
+    UInt_t lep_n;
+    UInt_t jet_n;
+    Float_t MET;
+    Float_t MET_phi;
+
+    Float_t lep_pt[10];
+    Float_t lep_eta[10];
+    Float_t lep_phi[10];
+    Float_t lep_E[10];
+    Int_t lep_type[10];
+    Float_t lep_ptcone30[10];
+    Float_t lep_etcone20[10];
+
+    Float_t jet_pt[10];
+    Float_t jet_eta[10];
+    Float_t jet_jvf[10];
+    Float_t jet_mv1[10];
+
+    tree->SetBranchAddress("trigE", &e_trig);
+    tree->SetBranchAddress("trigM", &mu_trig);
+    tree->SetBranchAddress("hasGoodVertex", &good_vtx);
+    tree->SetBranchAddress("lep_n", &lep_n);
+    tree->SetBranchAddress("jet_n", &jet_n);
+    tree->SetBranchAddress("met_et", &MET);
+    tree->SetBranchAddress("met_phi", &MET_phi);
+
+    tree->SetBranchAddress("lep_pt", &lep_pt);
+    tree->SetBranchAddress("lep_eta", &lep_eta);
+    tree->SetBranchAddress("lep_phi", &lep_phi);
+    tree->SetBranchAddress("lep_E", &lep_E);
+    tree->SetBranchAddress("lep_type", &lep_type);
+    tree->SetBranchAddress("lep_ptcone30", &lep_ptcone30);
+    tree->SetBranchAddress("lep_etcone20", &lep_etcone20);
+
+    tree->SetBranchAddress("jet_pt", &jet_pt);
+    tree->SetBranchAddress("jet_eta", &jet_eta);
+    tree->SetBranchAddress("jet_jvf", &jet_jvf);
+    tree->SetBranchAddress("jet_MV1", &jet_mv1);
+
+    //Canvas
+    TCanvas *canvas = new TCanvas("Canvas", "", 800, 600);
+
+    // Histograms
+    TH1F *cutflow = new TH1F("Cutflow", "Cutflow; Cut; Events", 10, 0, 10);
+
+    TH1F *hist_lepton_pt = new TH1F(
+        "Lepton pT", "Lepton pT; pT (GeV); Events",
+        50, 0, 1000);
+
+    TH1F *hist_lepton_pt_cone30 = new TH1F(
+        "Track isolation", "Track isolation; lep_ptcone30/lep_pt; Events",
+        50, 0, 0.2);
+
+    TH1F *hist_lepton_et_cone20 = new TH1F(
+        "Calorimeter isolation",
+        "Calorimeter isolation; lep_etcone30/lep_pt; Events",
+        50, 0, 0.2);
+
+    TH1F *hist_lepton_eta = new TH1F(
+        "Lepton eta", "Lepton eta; eta; Events",
+        50, -5, 5);
+
+    TH1F *hist_njets = new TH1F(
+        "Number of jets", "n-jets; Number of Jets; Events",
+        10, 0, 10);
+
+    TH1F *hist_jets_pt = new TH1F(
+        "Jets pT", "Jet pT; pT (GeV); Events",
+        50, 0, 1000);
+
+    TH1F *hist_jets_eta = new TH1F(
+        "Jets eta", "Jet eta; eta; Events",
+        50, -5, 5);
+
+    TH1F *hist_jets_JVF = new TH1F(
+        "Jets JVF", "Jet JVF; JVF; Events",
+        50, 0, 1);
+
+    TH1F *hist_jets_MV1 = new TH1F(
+        "Jets MV1", "Jets MV1; MV1; Events",
+        50, 0.5, 1);
+
+    TH1F *hist_nbjets = new TH1F(
+        "Number of b-jets", "n-bjets; Number of b-Jet; Events",
+        50, 0, 6);
+
+    TH1F *hist_MET = new TH1F(
+        "MET", "MET; MET (GeV); Events",
+        50, 0, 200);
+
+    TH1F *hist_mTW = new TH1F(
+        "mTW", "mTW; mTW (GeV); Events",
+        50, 0, 200);
+
+    int nentries, nbytes, i;
+    nentries = (Int_t)tree->GetEntries();
+
+    int cut1 = 0;
+    int cut2 = 0;
+    int cut3 = 0;
+    int cut4 = 0;
+    int cut5 = 0;
+    int cut6 = 0;
+    int cut7 = 0;
+    int cut8 = 0;
+
+    for (i = 0; i < nentries; i++)
+    {
+        nbytes = tree->GetEntry(i);
+        std::cout << nentries - i << "\n";
+
+        //First cut: Good vertex
+        if (!good_vtx)
+            continue;
+        cut1++;
+        cutflow->Fill(1);
+
+        //Second cut: Trigger
+        if (!e_trig && !mu_trig)
+            continue;
+        cut2++;
+        cutflow->Fill(2);
+
+        // Preselection of good leptons
+        int n_mu = 0;
+        int n_el = 0;
+        int n_lep = 0;
+        int g_lep = 0;
+
+        //Loop over leptons
+        for (unsigned int j = 0; j < lep_n; j++)
+        {
+            hist_lepton_pt->Fill(lep_pt[j] / 1000);
+            hist_lepton_pt_cone30->Fill(lep_ptcone30[j] / lep_pt[j]);
+            hist_lepton_et_cone20->Fill(lep_etcone20[j] / lep_pt[j]);
+            hist_lepton_eta->Fill(lep_eta[j]);
+
+            if (lep_pt[j] < 25000.0)
+                continue;
+
+            if (lep_ptcone30[j] / lep_pt[j] > 0.15)
+                continue;
+
+            if (lep_etcone20[j] / lep_pt[j] > 0.15)
+                continue;
+
+            if (lep_type[j] == 13 && TMath::Abs(lep_eta[j]) < 2.5)
+            {
+                n_mu++;
+                g_lep = j;
+            }
+
+            if (lep_type[j] == 11 && TMath::Abs(lep_eta[j]) < 2.47)
+            {
+                if (TMath::Abs(lep_eta[j]) < 1.37 || TMath::Abs(lep_eta[j]) > 1.52)
+                {
+                    n_el++;
+                    g_lep = j;
+                }
+            }
+            n_lep++;
+        }
+
+        //Third cut: One good Lepton
+        if (n_lep != 1)
+            continue;
+        cutflow->Fill(3);
+        cut3++;
+
+        int n_jets = 0;
+        int n_bjets = 0;
+
+        //Number of jets distribution
+        hist_njets->Fill(jet_n);
+
+        //Fourth cut: At least 4 jets
+        if (jet_n < 4)
+            continue;
+        cutflow->Fill(4);
+        cut4++;
+
+        for (unsigned int j = 0; j < jet_n; j++)
+        {
+            hist_jets_pt->Fill(jet_pt[j] / 1000);
+            hist_jets_eta->Fill(jet_eta[j]);
+            hist_jets_JVF->Fill(jet_jvf[j]);
+            hist_jets_MV1->Fill(jet_mv1[j]);
+
+            // To complete: apply jet cuts to find the good jets
+            if (jet_pt[j] < 25000.)
+                continue;
+
+            //Eta cut
+            if (jet_eta[j] > 2.5)
+                continue;
+
+            // JVF cleaning
+            if (jet_pt[j] < 50000. && TMath::Abs(jet_eta[j]) < 2.4)
+            {
+                if (jet_jvf[j] < 0.5)
+                    continue;
+            }
+
+            n_jets++;
+
+            // cut on 0.7892 MV1 and count the number of b-jets
+            if (jet_mv1[j] < 0.7892)
+                continue;
+
+            n_bjets++;
+        }
+
+        //Fifth cut: At least 4 good jets
+        if (n_jets < 4)
+            continue;
+        cutflow->Fill(5);
+        cut5++;
+
+        //Sixth cut: at least one b-jet
+        hist_nbjets->Fill(n_bjets);
+        if (n_bjets < 2)
+            continue;
+        cutflow->Fill(6);
+        cut6++;
+
+        //Seventh cut: MET > 30 GeV
+        hist_MET->Fill(MET / 1000);
+        if (MET < 30000.)
+            continue;
+        cutflow->Fill(7);
+        cut7++;
+
+        // TLorentzVector definitions
+        TLorentzVector Lepton = TLorentzVector();
+        Lepton.SetPtEtaPhiE(
+            lep_pt[g_lep],
+            lep_eta[g_lep],
+            lep_phi[g_lep],
+            lep_E[g_lep]);
+
+        TLorentzVector MeT = TLorentzVector();
+        MeT.SetPtEtaPhiE(
+            MET,
+            0,
+            MET_phi,
+            MET);
+
+        //Calculation of the mTW using TLorentz vectors
+        float mTW = sqrt(2 * Lepton.Pt() * MeT.Et() * (1 - cos(Lepton.DeltaPhi(MeT))));
+
+        //Eight cut: mTW > 30 GeV
+        hist_mTW->Fill(mTW / 1000);
+        if (mTW < 30000.)
+            continue;
+        cutflow->Fill(8);
+        cut8++;
+    }
+
+    std::cout << "Done!" << std::endl;
+    std::cout << "All events:" << nentries << std::endl;
+    std::cout << "Cut1:" << cut1 << std::endl;
+    std::cout << "Cut2:" << cut2 << std::endl;
+    std::cout << "Cut3:" << cut3 << std::endl;
+    std::cout << "Cut4:" << cut4 << std::endl;
+    std::cout << "Cut5:" << cut5 << std::endl;
+    std::cout << "Cut6:" << cut6 << std::endl;
+    std::cout << "Cut7:" << cut7 << std::endl;
+    std::cout << "Cut8:" << cut8 << std::endl;
+
+    canvas->SetLogy();
+    cutflow->SetFillColor(kGreen);
+    cutflow->Draw();
+    canvas->Print("cutflow.pdf");
+    canvas->Clear();
+
+    hist_lepton_pt->SetFillColor(kGreen);
+    hist_lep_pt->Draw();
+    canvas->Print("hist_lepton_pt.pdf");
+    canvas->Clear();
+
+    hist_lepton_pt_cone30->SetFillColor(kGreen);
+    hist_lepton_pt_cone30->Draw();
+    canvas->Print("hist_lepton_pt_cone30.pdf");
+    canvas->Clear();
+
+    hist_lepton_et_cone20->SetFillColor(kGreen);
+    hist_lepton_et_cone20->Draw();
+    canvas->Print("hist_lepton_et_cone20.pdf");
+    canvas->Clear();
+
+    hist_lepton_eta->SetFillColor(kGreen);
+    hist_lepton_eta->Draw();
+    canvas->Print("hist_lepton_eta.pdf");
+    canvas->Clear();
+
+    hist_njets->SetFillColor(kGreen);
+    hist_njets->Draw();
+    canvas->Print("hist_njets.pdf");
+    canvas->Clear();
+
+    hist_jets_pt->SetFillColor(kGreen);
+    hist_jets_pt->Draw();
+    canvas->Print("hist_jet_pt.pdf");
+    canvas->Clear();
+
+    hist_jets_eta->SetFillColor(kGreen);
+    hist_jets_eta->Draw();
+    canvas->Print("hist_jet_eta.pdf");
+    canvas->Clear();
+
+    hist_jets_JVF->SetFillColor(kGreen);
+    hist_jets_JVF->Draw();
+    canvas->Print("hist_jet_JVF.pdf");
+    canvas->Clear();
+
+    hist_jets_MV1->SetFillColor(kGreen);
+    hist_jets_MV1->Draw();
+    canvas->Print("hist_jet_MV1.pdf");
+    canvas->Clear();
+
+    hist_nbjets->SetFillColor(kGreen);
+    hist_nbjets->Draw();
+    canvas->Print("hist_nbjets.pdf");
+    canvas->Clear();
+
+    canvas->SetLogy(false);
+    hist_MET->SetFillColor(kGreen);
+    hist_MET->Draw();
+    canvas->Print("hist_MET.pdf");
+    canvas->Clear();
+
+    hist_mTW->SetFillColor(kGreen);
+    hist_mTW->Draw();
+    canvas->Print("hist_mTW.pdf");
+    canvas->Clear();
+}
